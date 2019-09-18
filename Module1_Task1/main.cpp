5210a3c4b3c557c8132a68347b4213c8f770335b
Еременко Илья <TheNeonLightning@yandex.ru>
2019-09-19 01:11:43 +0300

    Issues fixing

--- Module1_Task1/main.cpp
+++ Module1_Task1/main.cpp
@@ -14,48 +14,36 @@ using std::cout;
 using std::string;
 using std::vector;
 
+size_t PrefixFunctionNextValue(const string& line, vector<int>& prefix_function, size_t current_index, size_t previous_value) {
+  bool flag = false;
+  while (!flag) {
+    if (line[current_index] == line[previous_value]) {
+      previous_value = previous_value + 1;
+      flag = true;
+    } else {
+      if (previous_value == 0) {
+        flag = true;
+      }
+      previous_value = prefix_function[previous_value - 1];
+    }
+  }
+  return previous_value;
+}
+
+void PrefixFunctionForPattern(const string& line, vector<int>& prefix_function, size_t size) {
+  for (size_t index = 1; index < size; ++index) {
+    prefix_function[index] = PrefixFunctionNextValue(line, prefix_function, index, prefix_function[index - 1]);
+  }
+}
+
 void PositionSearch(vector<int>& result, const string& line, size_t size) {
   vector<int> prefix_function(size); // значение префикс-функции для паттерна
   prefix_function[0] = 0;
-  size_t current_index = 0; // индекс, который указывает на символ, с
-  // которым сравнивается текущий рассматриваемый (в описаниях алгоритма обычно
-  // обозначается как j)
-  for (size_t index = 1; index < size; ++index) { // вычисление префикс функции
-    // для паттерна
-    current_index = prefix_function[index - 1];
-    bool flag = false;
-    while (!flag) {
-      if (line[index] == line[current_index]) {
-        prefix_function[index] = current_index + 1;
-        flag = true;
-      } else {
-        if (current_index == 0) { // если дошли до первого символа и не нашли
-          // совпадения, то значение префикс функции равно 0
-          prefix_function[index] = 0;
-          flag = true;
-        }
-        current_index = prefix_function[current_index - 1];
-      }
-    }
-  }
-  size_t previous_value = 0;
+  PrefixFunctionForPattern(line, prefix_function, size);
+  size_t previous_value = 0; // достаточно хранить предыдущее значение префикс функции
   for (size_t index = size + 1; index < line.length(); ++index) { // вычисление
     // префикс функции для остальной части общей строки (без сохранения)
-    current_index = previous_value; // достаточно зранить только значение
-    // префикс функции на предыдущем символе
-    bool flag = false;
-    while (!flag) {
-      if (line[index] == line[current_index]) {
-        previous_value = current_index + 1;
-        flag = true;
-      } else {
-        if (current_index == 0) {
-          previous_value = 0;
-          flag = true;
-        }
-        current_index = prefix_function[current_index - 1];
-      }
-    }
+    previous_value = PrefixFunctionNextValue(line, prefix_function, index, previous_value);
     if (previous_value == size) { // если значение префикс функции равно длине
       // паттерна => нашли ещё одно вхождение
       result.push_back(index - 2 * size);
