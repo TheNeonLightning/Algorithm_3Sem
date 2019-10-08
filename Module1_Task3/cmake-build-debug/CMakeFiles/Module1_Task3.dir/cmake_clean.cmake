a37e27b02f42161c98719b0119d4143fef0da3ea
Еременко Илья <TheNeonLightning@yandex.ru>
2019-10-09 02:24:03 +0300

    Module one task three.

---
+++ Module1_Task3/cmake-build-debug/CMakeFiles/Module1_Task3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module1_Task3.dir/main.cpp.o"
+  "Module1_Task3"
+  "Module1_Task3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Module1_Task3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
